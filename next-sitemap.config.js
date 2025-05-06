c9e28f40f18b9d3eddb671391d6fabf09300fe30
Rohith S Moolya <smoolyarohith@gmail.com>
2025-05-06 11:48:53 +0530

    update of robots.txt,sitemap.xml for seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://nft99.vercel.app', // Change to your custom domain if you have one
+    generateRobotsTxt: true, // (optional)
+    sitemapSize: 7000,
+  };
+  
